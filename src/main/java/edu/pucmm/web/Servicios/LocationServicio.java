07fdcd35e94d7ddd68e1fc97636cce6555a999b3
Samir Comprés <samirant15@gmail.com>
2019-04-04 13:34:02 -0400

    fix de ip con shadowJar

--- src/main/java/edu/pucmm/web/Servicios/LocationServicio.java
+++ src/main/java/edu/pucmm/web/Servicios/LocationServicio.java
@@ -4,14 +4,16 @@ import com.maxmind.geoip2.DatabaseReader;
 
 import java.io.File;
 
+import static jdk.jfr.internal.SecuritySupport.getResourceAsStream;
+
 public class LocationServicio {
     private static LocationServicio instancia;
     public static DatabaseReader locationReader;
 
     private LocationServicio() {
-
-        File database = new File("src"+File.separator+"main"+File.separator+"resources"+File.separator+"Location"+File.separator+"GeoLite2-City.mmdb");
-        try {locationReader = new DatabaseReader.Builder(database).build();}
+//        var database = getResourceAsStream(File.separator+"Location"+File.separator+"GeoLite2-City.mmdb");
+//        File database = new File("src"+File.separator+"main"+File.separator+"resources"+File.separator+"Location"+File.separator+"GeoLite2-City.mmdb");
+        try {locationReader = new DatabaseReader.Builder(getClass().getClassLoader().getResourceAsStream(File.separator+"Location"+File.separator+"GeoLite2-City.mmdb")).build();}
         catch (Exception e) {System.out.println(e);}
     }
 
